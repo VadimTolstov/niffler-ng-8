97c0e80acdd1e86da64198d5b2526a91fd1101cb
VadimTolstov <tolstov50@mail.ru>
2025-04-03 18:52:19 +0300

    lesson3.2_hw

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
@@ -1,4 +1,58 @@
 package guru.qa.niffler.page;
 
-public class FriendsPage {
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static com.codeborne.selenide.Selenide.*;
+
+@ParametersAreNonnullByDefault
+public class FriendsPage extends BasePage<FriendsPage> {
+    public static final String URL = CFG.frontUrl() + "people/friends";
+    private final SelenideElement friendsTab = $("a[aria-selected='true'][href='/people/friends']");
+    private final ElementsCollection friendsList = $$(".MuiTableRow-root");
+    private final SelenideElement emptyListFriends = $("#simple-tabpanel-friends:not(:has(tr))");
+
+    @Step("Проверяем, что загрузилась страница Друзей")
+    @Override
+
+    public FriendsPage checkThatPageLoaded() {
+        friendsTab.shouldHave(Condition.visible);
+        return this;
+    }
+
+    @Step("Проверить отображение списка друзей: {users}")
+    public FriendsPage checkFriends(String... users) {
+        for (String user : users) {
+            friendsList.shouldHave(CollectionCondition.anyMatch(
+                    "Элемент с текстом '" + user + "' не найден",
+                    el -> el.getText().contains(user)
+            ));
+        }
+        return this;
+    }
+
+    @Step("Проверяем отображение входящей заявки на дружбу от пользователя: {username}")
+    public FriendsPage checkIncomeFriendship(String username) {
+        $x(".//tr[.//p[text()='%s'] and .//button[text()='Accept'] and .//button[text()='Decline']]"
+                .formatted(username)).shouldHave(Condition.visible);
+        return this;
+    }
+
+    @Step("Проверяем отображение пользователя: {username} в списке друзей")
+    public FriendsPage checkFriend(String username) {
+        $x(".//tr[.//p[text()='%s'] and .//button[text()='Unfriend']]"
+                .formatted(username)).shouldHave(Condition.visible);
+        return this;
+    }
+
+    @Step("Проверяем, что у пользователя нет друзей")
+    public FriendsPage emptyListFriends() {
+        emptyListFriends.shouldHave(Condition.visible);
+        return this;
+    }
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -0,0 +1,56 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.extension.BrowserExtension;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.Type.*;
+
+@ExtendWith(BrowserExtension.class)
+public class FriendsWebTest {
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void friendShouldBePresentInFriendsTable(@UsersQueueExtension.UserType(WITH_FRIEND) UsersQueueExtension.StaticUser user) {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .doLogin(new MainPage(), user.username(), user.password())
+                .getHeaderComponent()
+                .toFriendsPage()
+                .checkFriend(user.friend());
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void friendsTableShouldBeEmptyForNewUser(@UsersQueueExtension.UserType(EMPTY) UsersQueueExtension.StaticUser user) {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .doLogin(new MainPage(), user.username(), user.password())
+                .getHeaderComponent()
+                .toFriendsPage()
+                .emptyListFriends();
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void incomeInvitationBePresentInFriendsTable(@UsersQueueExtension.UserType(WITH_INCOME_REQUEST) UsersQueueExtension.StaticUser user) {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .doLogin(new MainPage(), user.username(), user.password())
+                .getHeaderComponent()
+                .toFriendsPage()
+                .checkIncomeFriendship(user.income());
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void outcomeInvitationBePresentInAllPeoplesTable(@UsersQueueExtension.UserType(WITH_OUTCOME_REQUEST) UsersQueueExtension.StaticUser user) {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .doLogin(new MainPage(), user.username(), user.password())
+                .getHeaderComponent()
+                .toAllPeoplesPage()
+                .checkStatusWaiting(user.outcome());
+    }
+
+}
