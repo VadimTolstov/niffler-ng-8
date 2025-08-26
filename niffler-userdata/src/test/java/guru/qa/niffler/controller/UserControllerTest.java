7d8e7864e7be9daa4885b2145dea99c742931f74
VadimTolstov <tolstov50@mail.ru>
2025-08-26 20:41:07 +0300

    lesson19.1

--- niffler-userdata/src/test/java/guru/qa/niffler/controller/UserControllerTest.java
+++ niffler-userdata/src/test/java/guru/qa/niffler/controller/UserControllerTest.java
@@ -23,18 +23,18 @@ class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-//  @Sql(scripts = "/currentUserShouldBeReturned.sql")
-//  @Test
-//  void currentUserShouldBeReturned() throws Exception {
-//    mockMvc.perform(get("/internal/users/current")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .param("username", "dima")
-//            )
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$.username").value("dima"))
-//            .andExpect(jsonPath("$.fullname").value("Dmitrii Tuchs"))
-//            .andExpect(jsonPath("$.currency").value("RUB"))
-//            .andExpect(jsonPath("$.photo").isNotEmpty())
-//            .andExpect(jsonPath("$.photoSmall").isNotEmpty());
-//  }
+  @Sql(scripts = "/currentUserShouldBeReturned.sql")
+  @Test
+  void currentUserShouldBeReturned() throws Exception {
+    mockMvc.perform(get("/internal/users/current")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("username", "dima")
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.username").value("dima"))
+            .andExpect(jsonPath("$.fullname").value("Dmitrii Tuchs"))
+            .andExpect(jsonPath("$.currency").value("RUB"))
+            .andExpect(jsonPath("$.photo").isNotEmpty())
+            .andExpect(jsonPath("$.photoSmall").isNotEmpty());
+  }
 }
